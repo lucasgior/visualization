40cb088ca8da186dd44050e15a01920457d3ca0f
lucasgior <lucasg_03@hotmail.com>
2014-12-20 12:57:24 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
 $( function() {
   // init Isotope
   var $container = $('.isotope').isotope({
-    itemSelector: '.color-shape'
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
   });
-
-  // store filter for each group
-  var filters = {};
-
-  $('#filters').on( 'click', '.button', function() {
-    var $this = $(this);
-    // get group key
-    var $buttonGroup = $this.parents('.button-group');
-    var filterGroup = $buttonGroup.attr('data-filter-group');
-    // set filter for group
-    filters[ filterGroup ] = $this.attr('data-filter');
-    // combine filters
-    var filterValue = '';
-    for ( var prop in filters ) {
-      filterValue += filters[ prop ];
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
     }
-    // set filter for Isotope
+  };
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
     $container.isotope({ filter: filterValue });
   });
-
   // change is-checked class on buttons
   $('.button-group').each( function( i, buttonGroup ) {
     var $buttonGroup = $( buttonGroup );
